e36cba595b83c3d5d7b0c9d9e327a494c077d05e
Aman kumar jha <81789772+jhaamankumar@users.noreply.github.com>
2023-10-06 19:56:21 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://vercel.com/jhaamankumar/stackoverflow/HK4JRkXBqeNgbBQZechFWvDLAbEa",
+  baseURL: "https://stackoverflow-indol.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
